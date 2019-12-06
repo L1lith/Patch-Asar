8bdb9599072d220bab2101e282f545be9d43823c
unknown <lily@l1lith.com>
2019-12-06 04:58:26 -0700

    add support for .patch-execute files

---
+++ source/executePatches.js
@@ -0,0 +1,35 @@
+const {promisify} = require('util')
+const find = require('find')
+const {dirname, basename, extname, join} = require('path')
+const rimraf = promisify(require('rimraf'))
+const {writeFile, readFile, exists} = require('mz/fs')
+
+async function executePatches(workingFolder) {
+  const patchesToExecute = await new Promise(res => {
+    find.file(/.patch-execute$/, workingFolder, files => res(files))
+  })
+  for (let i = 0; i < patchesToExecute.length; i++) {
+    const patch = patchesToExecute[i]
+    const postExecute = join(dirname(patch), basename(patch, extname(patch)))
+    let output
+    try {
+      output = require(patch)
+    } catch(error) {
+      error.message = "The following error occured while trying to require a .patch-execute file:\n" + error.message
+      throw error
+    }
+    let input = null
+    if (await exists(postExecute))) {
+      input = (await readFile(postExecute)).toString()
+    }
+    if (typeof output == 'function') output = output(input)
+    if (output instanceof Promise) output = await output
+    if (typeof output != 'string' && output !== null) throw new Error("The file did not output null or a string")
+    if (output === null) output = ""
+    await rimraf(postExecute)
+    await writeFile(postExecute, output)
+    await rimraf(patch)
+  }
+}
+
+module.exports = executePatches
--- source/patchAsar.js
+++ source/patchAsar.js
@@ -6,11 +6,13 @@ const mkdirp = require('mkdirp-promise')
 const asar = require('asar')
 const copydir = promisify(require('copy-dir'))
 const {basename, extname, join} = require('path')
+const executePatches = require('./executePatches')
 
 async function patchAsar(asarFilePath, patchFolderPatch, options={}) {
   if (typeof options != 'object') throw new Error("Options must be an object or null.")
-  let {workingDirectory=null} = options || {}
+  let {workingDirectory=null, keepWorkingDirectory=false} = options || {}
   let outputPath = options.outputPath || options.outputFile || null
+  if (typeof keepWorkingDirectory != 'boolean') throw new Error("keepWorkingDirectory must be a boolean")
   if (typeof asarFilePath != 'string' || !asarFilePath.endsWith('.asar') || !(await isFile(asarFilePath))) throw new Error("Invalid Asar File Path")
   if (typeof patchFolderPatch != 'string' || !(await isDirectory(patchFolderPatch))) throw new Error("Invalid Patch Folder Path")
   if (outputPath !== null && (typeof outputPath != 'string' || await isDirectory(outputPath))) throw new Error("Invalid Output File Path")
@@ -22,8 +24,10 @@ async function patchAsar(asarFilePath, patchFolderPatch, options={}) {
   await mkdirp(workingDirectory)
   await asar.extractAll(asarFilePath, workingDirectory)
   await copydir(patchFolderPatch, workingDirectory)
+  await executePatches(workingDirectory)
   await rimraf(outputPath)
-  await await asar.createPackage(workingDirectory, outputPath)
+  await asar.createPackage(workingDirectory, outputPath)
+  if (keepWorkingDirectory === false) await rimraf(workingDirectory)
 }
 
 module.exports = patchAsar
