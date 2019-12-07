d1d8536eb9898c9a4b7fdc8aced3b969f80e9bce
unknown <lily@l1lith.com>
2019-12-06 17:13:17 -0700

    fix minor bug with .patch-execute files

--- source/executePatches.js
+++ source/executePatches.js
@@ -1,16 +1,16 @@
 const {promisify} = require('util')
 const find = require('find')
-const {dirname, basename, extname, join} = require('path')
+const {dirname, basename, extname, join, relative} = require('path')
 const rimraf = promisify(require('rimraf'))
 const {writeFile, readFile, exists} = require('mz/fs')
 
-async function executePatches(workingFolder) {
+async function executePatches(workingFolder, patchFolder) {
   const patchesToExecute = await new Promise(res => {
-    find.file(/.patch-execute$/, workingFolder, files => res(files))
+    find.file(/.patch-execute$/, patchFolder, files => res(files))
   })
   for (let i = 0; i < patchesToExecute.length; i++) {
     const patch = patchesToExecute[i]
-    const postExecute = join(dirname(patch), basename(patch, extname(patch)))
+    const postExecute = join(workingFolder, relative(patchFolder, dirname(patch)), basename(patch, extname(patch)))
     let output
     try {
       output = require(patch)
@@ -19,7 +19,7 @@ async function executePatches(workingFolder) {
       throw error
     }
     let input = null
-    if (await exists(postExecute)) {
+    if (typeof output == 'function' && await exists(postExecute)) {
       input = (await readFile(postExecute)).toString()
     }
     if (typeof output == 'function') output = output(input)
